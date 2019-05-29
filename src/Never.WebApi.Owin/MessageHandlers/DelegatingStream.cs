10e97b1ca205b0d37bbf1756e68b297e2c45616d
shelldudu <shelldudu@hotmail.com>
2019-05-29 13:47:59 +0800

    init

---
+++ src/Never.WebApi.Owin/MessageHandlers/DelegatingStream.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.Owin;
+using Never.Web.Encryptions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Never.WebApi.Owin.MessageHandlers
+{
+    /// <summary>
+    /// 对内容流进行授权阅读
+    /// </summary>
+    public class DelegatingStream : Stream
+    {
+        #region ctor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegatingStream"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="encryption">The encryption.</param>
+        public DelegatingStream(IOwinContext context, IContentEncryptor encryption)
+        {
+            this.encryption = encryption;
+            this.context = context;
+            this.stream = context.Response.Body;
+        }
+
+        #endregion ctor
+
+        #region field
+
+        private readonly Stream stream = null;
+        private readonly IOwinContext context;
+        private readonly IContentEncryptor encryption = null;
+
+        #endregion field
+
+        #region stream
+
+        /// <summary>
+        ///
+        /// </summary>
+        public override bool CanRead
+        {
+            get
+            {
+                return this.stream.CanRead;
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public override bool CanSeek
+        {
+            get
+            {
+                return this.stream.CanSeek;
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public override bool CanWrite
+        {
+            get
+            {
+                return this.stream.CanWrite;
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public override long Length
+        {
+            get
+            {
+                return this.stream.Length;
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public override long Position
+        {
+            get
+            {
+                return this.stream.Position;
+            }
+            set
+            {
+                this.stream.Position = value;
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public override void Flush()
+        {
+            this.stream.Flush();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <param name="origin"></param>
+        /// <returns></returns>
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return this.stream.Seek(offset, origin);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        public override void SetLength(long value)
+        {
+            this.stream.SetLength(value);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return this.stream.Read(buffer, offset, count);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (offset == 0)
+            {
+                var @byte = this.encryption.Encrypt(buffer);
+                this.stream.Write(@byte, 0, @byte.Length);
+                return;
+            }
+            else
+            {
+                var @byte = new byte[count];
+                Array.Copy(buffer, offset, @byte, 0, count);
+                @byte = this.encryption.Encrypt(@byte);
+                this.stream.Write(@byte, 0, @byte.Length);
+                return;
+            }
+
+            //var text = Encoding.UTF8.GetString(buffer);
+            //var data = Encoding.UTF8.GetBytes(encryption.Encrypt(text));
+            //this.stream.Write(data, 0, data.Length);
+        }
+
+        #endregion stream
+    }
+}
---
+++ src/Never.WebApi.Owin/StartupExtension.cs
@@ -0,0 +1,162 @@
+﻿using Never.WebApi.Owin.MessageHandlers;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+using Never.Web.Encryptions;
+
+namespace Never.WebApi.Owin
+{
+    /// <summary>
+    /// 启动扩展
+    /// </summary>
+    public static class StartupExtension
+    {
+        /// <summary>
+        /// The get method dictionary
+        /// </summary>
+        private static readonly IDictionary<string, Regex> getMethodDict = null;
+
+        #region ctor
+
+        static StartupExtension()
+        {
+            getMethodDict = new System.Collections.Concurrent.ConcurrentDictionary<string, Regex>(4 * System.Environment.ProcessorCount, 100);
+        }
+
+        #endregion ctor
+
+        #region api
+
+        /// <summary>
+        /// 启用api对数据解密
+        /// </summary>
+        /// <param name="appBuilder"></param>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="findSecurityModelCallback">指示如何在当前请求中找到相应的加密与解密的方法</param>
+        /// <returns></returns>
+        public static IAppBuilder UseOwinApiHttpSecurityHandler(this IAppBuilder appBuilder, HttpConfiguration configuration, Func<IOwinRequest, IContentEncryptor> findSecurityModelCallback)
+        {
+            if (configuration.Properties.ContainsKey("UseOwinApiHttpSecurityHandler"))
+                return appBuilder;
+
+            appBuilder.Use((c, x) =>
+            {
+                var encryption = findSecurityModelCallback == null ? null : findSecurityModelCallback(c.Request);
+                if (encryption == null)
+                    return x.Invoke();
+
+                if ((encryption as UnAuthorizeContentEncryptor) != null)
+                {
+                    var task = new TaskCompletionSource<HttpResponseMessage>();
+                    c.Set<int>("owin.ResponseStatusCode", 401);
+                    return c.Response.WriteAsync("Unauthorized");
+                }
+
+                /*uri*/
+                RewriteUri(c, encryption);
+
+                /*content*/
+                RewriteContent(c, encryption);
+
+                return x.Invoke();
+            });
+
+            configuration.Properties["UseOwinApiHttpSecurityHandler"] = "t";
+            return appBuilder;
+        }
+
+        /// <summary>
+        /// 重写Uri
+        /// </summary>
+        /// <param name="context">The request.</param>
+        /// <param name="encryption">解密的model</param>
+        private static void RewriteUri(IOwinContext context, IContentEncryptor encryption)
+        {
+            Regex reg = null;
+            if (!getMethodDict.TryGetValue(encryption.ParamKey, out reg))
+            {
+                reg = new Regex(string.Format("{0}=(?<security>[^&]*)", encryption.ParamKey), RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                getMethodDict[encryption.ParamKey] = reg;
+            }
+
+            context.Request.QueryString = new QueryString(reg.Replace(context.Request.QueryString.Value, o =>
+            {
+                return encryption.Decrypt(o.Groups["security"].Value);
+            }));
+        }
+
+        /// <summary>
+        /// 重写content
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="encryption">解密的model</param>
+        private static void RewriteContent(IOwinContext context, IContentEncryptor encryption)
+        {
+            /*输出加密的内容*/
+            var @byte = default(byte[]);
+            using (var memoryStream = new MemoryStream())
+            {
+                context.Request.Body.CopyTo(memoryStream);
+                @byte = memoryStream.ToArray();
+            }
+
+            context.Request.Body = new MemoryStream(encryption.Decrypt(@byte));
+
+            //using (var reader = new StreamReader(context.Request.Body))
+            //{
+            //    var data = encryption.Decrypt(reader.ReadToEnd());
+            //    context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(data));
+            //}
+
+            context.Response.Body = new DelegatingStream(context, encryption);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static Task CreateErrorResponse(this IOwinContext context)
+        {
+            return CreateErrorResponse(context, HttpStatusCode.Unauthorized, "Unauthorized");
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="statusCode"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static Task CreateErrorResponse(this IOwinContext context, HttpStatusCode statusCode, string message)
+        {
+            context.Set<int>("owin.ResponseStatusCode", (int)statusCode);
+            return context.Response.WriteAsync(message);
+        }
+
+        /// <summary>
+        /// 获取Ip
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static string GetIP(this IOwinContext context)
+        {
+            var ip = context.Request.Headers["X-Forwarded-For"] ?? context.Request.Headers["X-Real-Ip"] ?? context.Request.RemoteIpAddress;
+            return ip.IsIP() ? ip : string.Empty;
+        }
+
+        #endregion api
+    }
+}
